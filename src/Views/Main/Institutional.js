911a2472e87b918c3b7609e9ed6806ddae3e3b4f
lucasdifresco <71949466+lucasdifresco@users.noreply.github.com>
2023-05-10 18:59:51 -0300

    Institutional update

--- src/Views/Main/Institutional.js
+++ src/Views/Main/Institutional.js
@@ -4,9 +4,9 @@ export default function Function() {
 
     return (
         <div className="text-center ar-100 bg-dark-20">
-            <h1>Magikus Studio</h1>
-            <h5 className="">We make games. We make tools.</h5>
-            <p className="p-5 w-75 m-auto">Our mision is to help game developers write better and more readable code, and to help them write it faster</p>
+            <h1>Magikus Studios</h1>
+            <h5 className="">Crafting games and tools that inspire</h5>
+            <p className="p-5 w-75 m-auto">Our mission at Magikus Studios is to create immersive and engaging games that push the boundaries of what's possible in the gaming industry. We believe that games can be more than just entertainment and strive to create experiences that inspire and educate players. In addition to games, we also provide tools and assets for Unity developers to create their own games more efficiently and effectively.</p>
         </div>
     );
 }
